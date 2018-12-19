385ba3735ecb55680ba179978bade8e63d92f82e
liushengxi13689209566 <liushengxi13689209566@163.com>
2018-12-19 11:59:13 +0800

    update

--- demon/cloud_coroutine/coroutine.c
+++ demon/cloud_coroutine/coroutine.c
@@ -167,6 +167,9 @@ void coroutine_resume(struct schedule *S, int id)
 static void
 _save_stack(struct coroutine *C, char *top)
 {
+	printf("stack:%p \n", stack);
+	printf("top:%p \n", top);
+
 	char dummy = 0;
 	assert(top - &dummy <= STACK_SIZE);
 	if (C->cap < top - &dummy)
