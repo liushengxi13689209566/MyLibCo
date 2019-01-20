c60d310c557aab8bb7152f9a2e9c0357eb803b04
liushengxi13689209566 <liushengxi13689209566@163.com>
2019-01-20 22:34:11 +0800

    add network module

---
+++ src/Epoll.h
@@ -0,0 +1,21 @@
+/*************************************************************************
+	> File Name: Epoll.h
+	> Author: Liu Shengxi 
+	> Mail: 13689209566@163.com
+	> Created Time: 2019年01月20日 星期日 22时07分58秒
+ ************************************************************************/
+
+#ifndef _EPOLL_H
+#define _EPOLL_H
+namespace Tatttoo
+{
+
+// 类似于　　stCoEpoll_t *pEpoll;　结构　
+//这里的网络模型架构我还是想采取　
+// 一个线程一个事件循环 一个线程里面有制定的协程去处理所到的事件
+
+class Epoll
+{
+};
+} // namespace Tatttoo
+#endif
